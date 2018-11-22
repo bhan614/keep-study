31c58b8881f34286b59b5176cbbea83cde4d1a41
baihan001 <baihan001@ke.com>
2018-11-22 15:20:20 +0800

    bind

---
+++ 实现一个bind方法/index.js
@@ -0,0 +1,45 @@
+// 最终版
+Function.prototype.bind = Function.prototype.bind || function bind(thisArg){
+    if(typeof this !== 'function'){
+        throw new TypeError(this + ' must be a function');
+    }
+    var self = this;
+    var args = [].slice.call(arguments, 1);
+    var bound = function(){
+        var boundArgs = [].slice.call(arguments);
+        var finalArgs = args.concat(boundArgs);
+        if(this instanceof bound){
+            if(self.prototype){
+                function Empty(){}
+                Empty.prototype = self.prototype;
+                bound.prototype = new Empty();
+            }
+            var result = self.apply(this, finalArgs);
+            var isObject = typeof result === 'object' && result !== null;
+            var isFunction = typeof result === 'function';
+            if(isObject || isFunction){
+                return result;
+            }
+            return this;
+        }
+        else{
+            return self.apply(thisArg, finalArgs);
+        }
+    };
+    return bound;
+}
+
+var obj = {
+    name: 'test',
+};
+function original(a, b){
+    console.log('this', this); // original {}
+    console.log('typeof this', typeof this); // object
+    this.name = b;
+    console.log('name', this.name); // 2
+    console.log('this', this);  // original {name: 2}
+    console.log([a, b]); // 1, 2
+}
+var bound = original.bind(obj, 1);
+var newBoundResult = new bound(2);
+console.log(newBoundResult, 'newBoundResult'); // original {name: 2}
